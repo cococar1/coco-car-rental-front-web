dc26e5fbca7906fd236f211c981902d7ce851f21
jmontenegro <jamzdeveloper@gmail.com>
2023-10-12 15:48:36 -0500

    fix gpt

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,8 +9,10 @@ export default class MyDocument extends Document {
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />), 
+          enhanceApp: (App) => (props) => {
+            const StyledApp = () => <App {...props} />;
+            return sheet.collectStyles(<StyledApp />);
+          },
         });
 
       const initialProps = await Document.getInitialProps(ctx);
